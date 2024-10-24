6cd0cc6823db6338c84a11a1e763f97d5d90d92c
juanfranfdezGit <jfranromfdez@gmail.com>
2024-10-24 18:25:48 +0200

    cors

--- server.js
+++ server.js
@@ -19,10 +19,17 @@ const allowedOrigins = ['https://virtuallifefrontend.vercel.app'];
 app.use(cors({
   origin: allowedOrigins,
   methods: ['GET', 'POST', 'PUT', 'DELETE'], 
-  credentials: true 
+  credentials: true, 
+  exposedHeaders: ['Authorization'],
 }));
 // Para recibir JSON
 app.use(express.json());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", allowedOrigins);
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
 
 // Credenciales de nuestra Base de datos
 const db = mysql.createConnection({
