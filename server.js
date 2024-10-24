7a37aed8594053d317f7170afa595b91c69d8471
juanfranfdezGit <jfranromfdez@gmail.com>
2024-10-24 19:18:59 +0200

    afas

--- server.js
+++ server.js
@@ -13,30 +13,10 @@ const port = 3001;
 // Clave secreta para el Token
 const JWT_SECRET = 'virtuallife-sessions';
 
-const allowedOrigins = ['https://virtuallifefrontend.vercel.app'];
-
 // Llamamos a cors
-app.use(cors({
-  origin: allowedOrigins,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], 
-  credentials: true, 
-  exposedHeaders: ['Authorization'],
-}));
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  next();
-});
-
+app.use(cors());
 // Para recibir JSON
 app.use(express.json());
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", allowedOrigins);
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  next();
-});
 
 // Credenciales de nuestra Base de datos
 const db = mysql.createConnection({
